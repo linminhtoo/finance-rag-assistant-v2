3a845b911050db425041b6927a3133d5bd135f50
Lin Min Htoo <linmin.htoo@gmail.com>
2025-12-25 01:49:02 +0800

    chore

--- scripts/chunk.sh
+++ scripts/chunk.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # NOTE: script does logging set up internally. we do NOT need to `tee`
 source "$(dirname "${BASH_SOURCE[0]}")/_env.sh"
-python3 -m scripts.ingest \
+python3 -m scripts.chunk \
 	    --markdown-dir /home/mlin/repos/z_scratch/financial-rag/data/sec_filings/processed_markdown \
 	    --output-dir /home/mlin/repos/z_scratch/financial-rag/data/sec_filings/chunked_v1 \
 	    --preprocess-markdown-tables \
