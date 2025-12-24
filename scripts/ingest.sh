12fb092cbb923706019abeeb6088b3ffeae1a160
Lin Min Htoo <linmin.htoo@gmail.com>
2025-12-25 01:47:58 +0800

    feat(scripts): add initial bash script to chunk

---
+++ scripts/ingest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+
+# NOTE: script does logging set up internally. we do NOT need to `tee`
+source "$(dirname "${BASH_SOURCE[0]}")/_env.sh"
+python3 -m scripts.ingest \
+	    --markdown-dir /home/mlin/repos/z_scratch/financial-rag/data/sec_filings/processed_markdown \
+	    --output-dir /home/mlin/repos/z_scratch/financial-rag/data/sec_filings/chunked_v1 \
+	    --preprocess-markdown-tables \
+	    --recursive
+# TODO: check length of chunks, if too small, what should we do?
