c7204257b88f6801eabb5a7a2b6f5714aa184570
Lin Min Htoo <linmin.htoo@gmail.com>
2025-12-31 05:01:38 +0800

    feat(utils): add seed_everything()

--- src/finrag/utils.py
+++ src/finrag/utils.py
@@ -1,8 +1,19 @@
 import os
 
+from loguru import logger
+
 
 def get_env_var(name: str) -> str:
     value = os.environ.get(name)
     if not value:
         raise RuntimeError(f"environment variable: {name} is required but missing.")
     return value
+
+
+def seed_everything(seed: int) -> None:
+    import random
+    import numpy as np
+
+    random.seed(seed)
+    np.random.seed(seed)
+    logger.info(f"Random seed set to: {seed}")
